95a35ce720e6a0e1f22e736422d2053cad842014
Carlos Porto <carlosturop@gmail.com>
2022-11-02 20:42:12 -0500

    Carlos
    Se sube el contenido de componentes agregar y modificar con sus respectivos css

--- src/componentes/Admin-Agregar/agregar.js
+++ src/componentes/Admin-Agregar/agregar.js
@@ -1,13 +1,43 @@
 import React from "react";
-import { FaClipboardList, FaEdit, FaBook } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
+import { Plantilla } from "../Plantilla/plantilla";
+import "./agregar.css";
 
-export const HeaderAgregar = () =>{
+const HeaderAgregar = () =>{
     return(
-        <div>
-            <a href="#" className="menu"><FaClipboardList/> Lista de Productos </a>
-            <a href="#" className="menu"><FaEdit/> Modificar Productos </a>
-            <a href="#" className="menu"><FaBook/> Ventas </a>
+        
+        <div className="contenedor">
+            <Plantilla/>
+            <body className="body">
+                <h1 className="titulo"> Agregar Producto </h1>
+                <Link to="/" className="boton"><FaArrowAltCircleLeft/> </Link>
+                <form className="form">
+                    <div>
+                        <picture id="producto">
+                            <img src="#" alt="Producto" class="img_producto"/>
+                        </picture>
+                        <input  type="file" className="boton_subir"></input>
+                    </div>
+                    <div>
+                        <label className="nombre_campo">Nombre</label>
+                        <input type="text" className="campo"></input>
+
+                        <label className="nombre_campo">Descripción</label>
+                        <textarea className="campo descripcion"></textarea>
+
+                        <label className="nombre_campo">Precio</label>
+                        <input type="text" className="campo"></input>
+
+                        <label className="nombre_campo">Stock</label>
+                        <input type="number" className="campo"></input>
+
+                        <button type="submit" className="boton_form"> Guardar Cambios </button>
+                    </div>
+                </form>
+            </body>
         </div>
-    )
-    
+    ) 
 }
+
+export default HeaderAgregar
--- src/componentes/Admin-Modificar/modificar.js
+++ src/componentes/Admin-Modificar/modificar.js
@@ -1,13 +1,49 @@
 import React from "react";
-import { FaClipboardList, FaEdit, FaBook } from 'react-icons/fa';
+import { Plantilla } from "../Plantilla/plantilla";
+import "./modificar.css"
 
-export const HeaderModificar = () =>{
+const  HeaderModificar = () =>{
     return(
         <div>
-            <a href="#" className="menu"><FaClipboardList/> Lista de Productos </a>
-            <a href="#" className="menu"><FaEdit/> Modificar Productos </a>
-            <a href="#" className="menu"><FaBook/> Ventas </a>
+            <Plantilla/>
+            
+            <body className="body">
+                <h1 className="titulo"> Modificar Producto </h1>
+                
+
+                <form className="form">
+                    <div>
+                        <select className="campo" name="lista_productos">
+                            <option value="lista_productos">Producto1</option>
+                            <option value="lista_productos">Producto</option>
+                            <option value="lista_productos">Producto3</option>
+                        </select>
+                        <picture id="producto">
+                            <img src="#" alt="Producto" class="img_producto"/>
+                        </picture>
+                        <input  type="file" className="boton_subir"></input>
+                    </div>
+                    <div>
+                        <label className="nombre_campo">Nombre</label>
+                        <input type="text" className="campo"></input>
+
+                        <label className="nombre_campo">Descripción</label>
+                        <textarea className="campo descripcion"></textarea>
+
+                        <label className="nombre_campo">Precio</label>
+                        <input type="text" className="campo"></input>
+
+                        <label className="nombre_campo">Stock</label>
+                        <input type="number" className="campo"></input>
+
+                        <button type="submit" className="boton_form"> Guardar Cambios </button>
+                    </div>
+                </form>
+            </body>
+            
         </div>
     )
     
 }
+
+export default HeaderModificar
